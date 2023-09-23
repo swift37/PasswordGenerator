61e4fc90dd8824ae9df5163fa060eae121ed6432
Swift <swiftx37@gmail.com>
2023-09-23 18:58:49 +0200

    Scripts for new elements

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const passwordInput = document.querySelector('.password-container input')
 const lengthInput = document.querySelector('.range-container input')
+const passwordIndicator = document.querySelector('.password-indicator')
 const selectedLength = document.querySelector('.selected-length')
 const copyBtn = document.querySelector('.copy-button')
 const generateBtn = document.querySelector('.generate-button')
+const options = document.querySelectorAll('.option input')
 
 lengthInput.addEventListener('input', () => {
 	selectedLength.textContent = lengthInput.value
@@ -14,17 +16,51 @@ copyBtn.addEventListener('click', () => {
 	copyBtn.classList.replace('fa-copy', 'fa-circle-check')
 })
 
+const createCharsString = () => {
+	const chars = {
+		lowercase: 'abcdefghijklmnopqrstuvwxyz',
+		uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+		numbers: '0123456789',
+		symbols: '^!$%&|[](){}:;.,*+-#@<>~_/\\',
+	}
+
+	let charsString = ''
+	options.forEach(option => {
+		if (option.checked) charsString += chars[option.id]
+	})
+
+	return charsString
+}
+
+const updatePasswordIndicator = password => {
+	let strength = 0
+	if (password.match(/[a-z]+/)) strength += 1
+	if (password.match(/[A-Z]+/)) strength += 1
+	if (password.match(/[0-9]+/)) strength += 1
+	if (password.match(/[$@#&!*^%|:;.,+\-<>~_/\\{}()[\]]+/)) strength += 1
+	if (password.length >= 12) strength += 1
+	if (password.length < 8) strength -= 2
+	console.log(strength)
+	strength < 2
+		? (passwordIndicator.id = 'weak')
+		: strength < 4
+		? (passwordIndicator.id = 'medium')
+		: (passwordIndicator.id = 'strong')
+}
+
 const generatePassword = () => {
-	const allCharacter =
-		'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789^!$%&|[](){}:;.,*+-#@<>~'
+	const charsString = createCharsString()
 	let password = ''
 	for (i = 0; i < lengthInput.value; i++)
-		password += allCharacter[Math.floor(Math.random() * allCharacter.length)]
+		password += charsString[Math.floor(Math.random() * charsString.length)]
 
 	passwordInput.value = password
+	updatePasswordIndicator(password)
 	copyBtn.classList.replace('fa-circle-check', 'fa-copy')
 }
 
 generatePassword()
 
 generateBtn.addEventListener('click', generatePassword)
+
+options.forEach(element => element.addEventListener('change', generatePassword))
